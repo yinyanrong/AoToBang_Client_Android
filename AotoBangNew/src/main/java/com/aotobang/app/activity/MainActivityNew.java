4c1e2934bb1c39bd965976091b22f7fef1eafa60
yyr <450742455@qq.com>
2015-12-05 18:24:26 +0800

    整合

--- AotoBangNew/src/main/java/com/aotobang/app/activity/MainActivityNew.java
+++ AotoBangNew/src/main/java/com/aotobang/app/activity/MainActivityNew.java
@@ -1,86 +1,77 @@
 //package com.aotobang.app.activity;
 //
+//import android.app.Activity;
 //import android.os.Bundle;
-//import android.support.design.widget.NavigationView;
 //import android.support.v4.view.GravityCompat;
 //import android.support.v4.widget.DrawerLayout;
+//import android.support.v7.app.ActionBar;
+//import android.support.v7.app.ActionBarActivity;
 //import android.support.v7.app.AppCompatActivity;
+//import android.support.v7.widget.Toolbar;
+//import android.view.LayoutInflater;
 //import android.view.Menu;
 //import android.view.MenuItem;
-//import android.view.View;
+//import android.widget.ListView;
 //
-//import com.aotobang.R;
+///**
+// * Created by Administrator on 2015/12/3.
+// */
+//public class NavListViewActivity extends Activity
+//{
+//    private ListView mLvLeftMenu;
+//    private DrawerLayout mDrawerLayout;
 //
-//
-//public class MainActivityNew extends AppCompatActivity
-//        implements NavigationView.OnNavigationItemSelectedListener {
 //
 //    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
+//    protected void onCreate(Bundle savedInstanceState)
+//    {
 //        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_main);
+//        setContentView(R.layout.activity_nav_list_view);
 //
-//        final DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-//        findViewById(R.id.show_NavigationView).setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                drawer.openDrawer(GravityCompat.START);
-//            }
-//        });
-//        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-//        navigationView.setNavigationItemSelectedListener(this);
+//        mDrawerLayout = (DrawerLayout) findViewById(R.id.id_drawer_layout);
+//        mLvLeftMenu = (ListView) findViewById(R.id.id_lv_left_menu);
+//
+//        Toolbar toolbar = (Toolbar) findViewById(R.id.id_toolbar);
+//        setSupportActionBar(toolbar);
+//
+//        final ActionBar ab = getSupportActionBar();
+//        ab.setHomeAsUpIndicator(R.drawable.ic_menu_camera);
+//        ab.setDisplayHomeAsUpEnabled(true);
+//
+//        setUpDrawer();
+//    }
+//    private void setUpDrawer()
+//    {
+//        LayoutInflater inflater = LayoutInflater.from(this);
+//        mLvLeftMenu.addHeaderView(inflater.inflate(R.layout.nav_header_home, mLvLeftMenu, false));
+//        mLvLeftMenu.setAdapter(new MenuItemAdapter(this));
 //    }
 //
 //    @Override
-//    public void onBackPressed() {
-//        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-//        if (drawer.isDrawerOpen(GravityCompat.START)) {
-//            drawer.closeDrawer(GravityCompat.START);
-//        } else {
-//            super.onBackPressed();
-//        }
-//    }
-//
-//    @Override
-//    public boolean onCreateOptionsMenu(Menu menu) {
+//    public boolean onCreateOptionsMenu(Menu menu)
+//    {
 //        // Inflate the menu; this adds items to the action bar if it is present.
-//        getMenuInflater().inflate(R.menu.main, menu);
+////            getMenuInflater().inflate(R.menu.nav_header_home, menu);
 //        return true;
 //    }
 //
 //    @Override
-//    public boolean onOptionsItemSelected(MenuItem item) {
+//    public boolean onOptionsItemSelected(MenuItem item)
+//    {
+//        // Handle action bar item clicks here. The action bar will
+//        // automatically handle clicks on the Home/Up button, so long
+//        // as you specify a parent activity in AndroidManifest.xml.
 //        int id = item.getItemId();
 //
 //        //noinspection SimplifiableIfStatement
-//        if (id == R.id.action_settings) {
+//        if (id == android.R.id.home)
+//        {
+//            mDrawerLayout.openDrawer(GravityCompat.START);
 //            return true;
 //        }
-//
 //        return super.onOptionsItemSelected(item);
 //    }
 //
-//    @SuppressWarnings("StatementWithEmptyBody")
-//    @Override
-//    public boolean onNavigationItemSelected(MenuItem item) {
-//        // Handle navigation view item clicks here.
-//        int id = item.getItemId();
 //
-//        if (id == R.id.nav_camera) {
-//            // Handle the camera action
-//        } else if (id == R.id.nav_gallery) {
 //
-//        } else if (id == R.id.nav_slideshow) {
-//
-//        } else if (id == R.id.nav_manage) {
-//
-//        } else if (id == R.id.nav_share) {
-//
-//        } else if (id == R.id.nav_send) {
-//
-//        }
-//        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-//        drawer.closeDrawer(GravityCompat.START);
-//        return true;
-//    }
 //}
